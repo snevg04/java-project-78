1a9d1dbcafd90cb4bf16b70b1db2de6fc5ef7117
Evgenii <evg.sneg.117@yandex.ru>
2026-03-18 11:27:45 +1000

    set build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,9 @@
 plugins {
-    id("java")
+    id("com.github.ben-manes.versions") version "0.51.0"
+    id("org.sonarqube") version "7.1.0.6387"
+    application
+    jacoco
+    checkstyle
 }
 
 group = "hexlet.code"
@@ -15,6 +19,30 @@ dependencies {
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
-tasks.test {
+application {
+    mainClass.set("hexlet.code.App")
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "snevg04_java-project-71")
+        property("sonar.organization", "sn-evg-04")
+        property(
+            "sonar.coverage.jacoco.xmlReportPaths",
+            "${layout.buildDirectory.get()}/reports/jacoco/test/jacocoTestReport.xml"
+        )
+    }
+}
+
+tasks.named<Test>("test") {
     useJUnitPlatform()
+}
+
+tasks.named<JacocoReport>("jacocoTestReport") {
+    dependsOn(tasks.test)
+    reports {
+        xml.required.set(true)
+        html.required.set(true)
+        csv.required.set(false)
+    }
 }
