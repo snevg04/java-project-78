23594a9b673b88794652aca2a4c7eb1fb6603d2e
Evgenii <evg.sneg.117@yandex.ru>
2026-03-20 13:12:41 +1000

    add App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,4 +1,10 @@
 package hexlet.code;
 
 public class App {
+    public static void main(String[] args) {
+
+        var v = new Validator();
+        var schema = v.string();
+        System.out.println(schema.required().contains("What").isValid("Hello there"));
+    }
 }
