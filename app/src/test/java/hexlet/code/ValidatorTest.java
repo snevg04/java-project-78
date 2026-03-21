cad3d26a0d0cc7fa42e58a402b5fb438b2641ef0
Evgenii <evg.sneg.117@yandex.ru>
2026-03-21 15:16:17 +1000

    add NumberSchema tests

--- app/src/test/java/hexlet/code/ValidatorTest.java
+++ app/src/test/java/hexlet/code/ValidatorTest.java
@@ -1,22 +1,24 @@
 package hexlet.code;
 
+import hexlet.code.schemas.NumberSchema;
+import hexlet.code.schemas.StringSchema;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
-import hexlet.code.schemas.BaseSchema;
 
 public class ValidatorTest {
 
-    private static BaseSchema schema;
+    private static Validator v;
 
     @BeforeEach
     public void makeValidatorString() {
-        schema = new Validator().string();
+        v = new Validator();
     }
 
     @Test
     public void noParamNullTest() {
 
+        StringSchema schema = v.string();
         boolean actual = schema.isValid(null);
         boolean expected = true;
         Assertions.assertEquals(expected, actual);
@@ -25,6 +27,7 @@ public class ValidatorTest {
     @Test
     public void noParamEmptyTest() {
 
+        StringSchema schema = v.string();
         boolean actual = schema.isValid("");
         boolean expected = true;
         Assertions.assertEquals(expected, actual);
@@ -33,6 +36,7 @@ public class ValidatorTest {
     @Test
     public void requiredNullTest() {
 
+        StringSchema schema = v.string();
         boolean actual = schema.required().isValid(null);
         boolean expected = false;
         Assertions.assertEquals(expected, actual);
@@ -41,6 +45,7 @@ public class ValidatorTest {
     @Test
     public void requiredEmptyTest() {
 
+        StringSchema schema = v.string();
         boolean actual = schema.required().isValid("");
         boolean expected = false;
         Assertions.assertEquals(expected, actual);
@@ -49,6 +54,7 @@ public class ValidatorTest {
     @Test
     public void minLengthWrongTest() {
 
+        StringSchema schema = v.string();
         boolean actual = schema.minLength(7).isValid("Hello");
         boolean expected = false;
         Assertions.assertEquals(expected, actual);
@@ -57,6 +63,7 @@ public class ValidatorTest {
     @Test
     public void minLengthRightTest() {
 
+        StringSchema schema = v.string();
         boolean actual = schema.minLength(4).isValid("Hello");
         boolean expected = true;
         Assertions.assertEquals(expected, actual);
@@ -65,6 +72,7 @@ public class ValidatorTest {
     @Test
     public void containsRightTest() {
 
+        StringSchema schema = v.string();
         boolean actual = schema.contains("Hel").isValid("Hello");
         boolean expected = true;
         Assertions.assertEquals(expected, actual);
@@ -73,6 +81,7 @@ public class ValidatorTest {
     @Test
     public void containsWrongTest() {
 
+        StringSchema schema = v.string();
         boolean actual = schema.contains("What").isValid("Hello");
         boolean expected = false;
         Assertions.assertEquals(expected, actual);
@@ -81,6 +90,7 @@ public class ValidatorTest {
     @Test
     public void containsMinLengthRightTest() {
 
+        StringSchema schema = v.string();
         boolean actual = schema.minLength(4).contains("Hel").isValid("Hello");
         boolean expected = true;
         Assertions.assertEquals(expected, actual);
@@ -89,6 +99,7 @@ public class ValidatorTest {
     @Test
     public void containsMinLengthWrongTest() {
 
+        StringSchema schema = v.string();
         boolean actual = schema.minLength(8).contains("What").isValid("Hello");
         boolean expected = false;
         Assertions.assertEquals(expected, actual);
@@ -97,6 +108,7 @@ public class ValidatorTest {
     @Test
     public void requiredMinLengthContainsRightTest() {
 
+        StringSchema schema = v.string();
         boolean actual = schema.required().minLength(4).contains("Hel").isValid("Hello");
         boolean expected = true;
         Assertions.assertEquals(expected, actual);
@@ -105,6 +117,7 @@ public class ValidatorTest {
     @Test
     public void requiredMinLengthContainsWrongTest() {
 
+        StringSchema schema = v.string();
         boolean actual = schema.required().minLength(8).contains("What").isValid("Hello");
         boolean expected = false;
         Assertions.assertEquals(expected, actual);
@@ -113,7 +126,71 @@ public class ValidatorTest {
     @Test
     public void requiredMinLengthContainsNullTest() {
 
+        StringSchema schema = v.string();
         boolean actual = schema.required().minLength(8).contains("What").isValid(null);
+        boolean expected = false;
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void numberNullTest() {
+
+        NumberSchema schema = v.number();
+        boolean actual = schema.required().isValid(null);
+        boolean expected = false;
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void numberPositiveRightTest() {
+
+        NumberSchema schema = v.number();
+        boolean actual = schema.positive().isValid(5);
+        boolean expected = true;
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void numberPositiveWrongTest() {
+
+        NumberSchema schema = v.number();
+        boolean actual = schema.positive().isValid(-5);
+        boolean expected = false;
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void numberPositiveZeroTest() {
+
+        NumberSchema schema = v.number();
+        boolean actual = schema.positive().isValid(0);
+        boolean expected = false;
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void numberRangeRightTest() {
+
+        NumberSchema schema = v.number();
+        boolean actual = schema.range(5, 10).isValid(7);
+        boolean expected = true;
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void numberRangeWrongTest() {
+
+        NumberSchema schema = v.number();
+        boolean actual = schema.range(5, 10).isValid(15);
+        boolean expected = false;
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void numberRequiredPositiveRangeRightTest() {
+
+        NumberSchema schema = v.number();
+        boolean actual = schema.required().positive().range(5, 10).isValid(-5);
         boolean expected = false;
         Assertions.assertEquals(expected, actual);
     }
