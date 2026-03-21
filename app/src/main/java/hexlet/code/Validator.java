66b04f7659837b064ce2a77598417240f5d2f58c
Evgenii <evg.sneg.117@yandex.ru>
2026-03-21 15:15:24 +1000

    update Validator

--- app/src/main/java/hexlet/code/Validator.java
+++ app/src/main/java/hexlet/code/Validator.java
@@ -1,11 +1,15 @@
 package hexlet.code;
 
-import hexlet.code.schemas.BaseSchema;
+import hexlet.code.schemas.NumberSchema;
 import hexlet.code.schemas.StringSchema;
 
 public class Validator {
 
-    public BaseSchema string() {
+    public StringSchema string() {
         return new StringSchema();
+    }
+
+    public NumberSchema number() {
+        return new NumberSchema();
     }
 }
