b71c868fa931fa6ccea38396e18b3e4de08afa23
Evgenii <evg.sneg.117@yandex.ru>
2026-03-21 15:14:55 +1000

    add NumberSchema extending BaseSchema and add logic

---
+++ app/src/main/java/hexlet/code/schemas/NumberSchema.java
@@ -0,0 +1,47 @@
+package hexlet.code.schemas;
+
+public class NumberSchema extends BaseSchema<Integer> {
+
+    private boolean positive;
+    private Integer minRange;
+    private Integer maxRange;
+
+    public NumberSchema required() {
+        required = true;
+        return this;
+    }
+
+    public NumberSchema positive() {
+        positive = true;
+        return this;
+    }
+
+    public NumberSchema range(int min, int max) {
+
+        minRange = min;
+        maxRange = max;
+
+        return this;
+    }
+
+    public boolean isValid(Integer number) {
+
+        if (required) {
+            if (number == null) {
+                return false;
+            }
+        }
+
+        if (positive) {
+            if (number <= 0) {
+                return false;
+            }
+        }
+
+        if (minRange != null && (number < minRange || number > maxRange)) {
+            return false;
+        }
+
+        return true;
+    }
+}
