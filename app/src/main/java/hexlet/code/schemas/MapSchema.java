56ff3c881f76d50e5af47255200fb906d65edc48
Evgenii <evg.sneg.117@yandex.ru>
2026-03-22 15:42:45 +1000

    add MapSchema logic

--- app/src/main/java/hexlet/code/schemas/MapSchema.java
+++ app/src/main/java/hexlet/code/schemas/MapSchema.java
@@ -1,4 +1,35 @@
 package hexlet.code.schemas;
 
-public class MapSchema {
+import java.util.Map;
+
+public class MapSchema extends BaseSchema<Map> {
+
+    private int sizeOf;
+
+    public MapSchema required() {
+        required = true;
+        return this;
+    }
+
+    public MapSchema sizeOf(int number) {
+        this.sizeOf = number;
+        return this;
+    }
+
+    public boolean isValid(Map map) {
+
+        if (required) {
+            if (map == null) {
+                return false;
+            }
+        }
+
+        if (sizeOf > 0) {
+            if (map.size() < sizeOf) {
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
