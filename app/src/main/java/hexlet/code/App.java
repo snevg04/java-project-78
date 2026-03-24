84f40d391a711acdf9d712d73d79dbdf55d06208
Evgenii <evg.sneg.117@yandex.ru>
2026-03-24 15:47:12 +1000

    remove temporal system.out check line

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,9 +2,5 @@ package hexlet.code;
 
 public class App {
     public static void main(String[] args) {
-
-        var v = new Validator();
-        var schema = v.string();
-        System.out.println(schema.required().contains("What").isValid("Hello there"));
     }
 }
