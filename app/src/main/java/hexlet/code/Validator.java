fcad5ed5ca9d3cd3186b91295951360bd7996eb3
Evgenii <evg.sneg.117@yandex.ru>
2026-03-24 15:41:39 +1000

    add ShapeSchema and its import to Validator

--- app/src/main/java/hexlet/code/Validator.java
+++ app/src/main/java/hexlet/code/Validator.java
@@ -2,6 +2,7 @@ package hexlet.code;
 
 import hexlet.code.schemas.MapSchema;
 import hexlet.code.schemas.NumberSchema;
+import hexlet.code.schemas.ShapeSchema;
 import hexlet.code.schemas.StringSchema;
 
 public class Validator {
