9f682a2b1700a77147745cccbf43839e472cd89c
snevg04 <98708452+snevg04@users.noreply.github.com>
2026-04-03 13:03:05 +1000

    Update NumberSchema.java

--- app/src/main/java/hexlet/code/schemas/NumberSchema.java
+++ app/src/main/java/hexlet/code/schemas/NumberSchema.java
@@ -9,12 +9,12 @@ public final class NumberSchema extends BaseSchema<Integer> {
     }
 
     public NumberSchema positive() {
-        addCheck("positive", value -> value != null && value > 0);
+        addCheck("positive", value -> value == null && value > 0);
         return this;
     }
 
     public NumberSchema range(int min, int max) {
-        addCheck("range", value -> value != null && (value >= min && value <= max));
+        addCheck("range", value -> value == null || (value >= min && value <= max));
         return this;
     }
 }
